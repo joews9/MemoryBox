b9c2ed65377775c806cbfed777ffd5606f85f6af
joews9 <millershipj@yahoo.co.uk>
2016-04-25 14:32:41 +0100

    Update AddMemoryFragment.java
    ft78tf

--- app/src/main/java/com/event/joe/myapplication/AddMemoryFragment.java
+++ app/src/main/java/com/event/joe/myapplication/AddMemoryFragment.java
@@ -16,6 +16,7 @@ public class AddMemoryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.add_memory_layout, container, false);
+        //new comment
         return view;
 
     }
